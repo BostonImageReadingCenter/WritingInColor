61bfbb40f65d06d514a94768d07fa97bbc261400
Hudson Gouge <hudson.gouge@icloud.com>
2024-06-07 15:29:43 -0400

    setup files and folders

---
+++ server.js
@@ -0,0 +1,9 @@
+// Koa vs Fastify vs Express
+// Koa: https://github.com/koajs/koa
+// Fastify: https://github.com/fastify/fastify
+// Express: https://github.com/expressjs/express
+
+// Fastify is the fastest, Koa is second, and Express is by far the slowest.
+// Koa is the most customizable.
+// Express is the one I have the most experience with.
+// Fastify is fast.
